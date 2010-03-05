ab5a7490496b622b45620b2bf2c3d5870a9323d5
Mark Evans <mark@new-bamboo.co.uk>
2010-03-05 10:14:29 +0000

    Committed to github because Makoto asked me to!

---
+++ jquery.objectify.js
@@ -0,0 +1,75 @@
+(function(){
+  
+  // Taken from http://github.com/markevans/jquery-objectify
+  // See the README for usage instructions
+ 
+  $.objectifications = [];
+ 
+  $.objectify = function(name, opts){
+    
+    // 'prototype_vars' correspond to both methods and attributes
+    var prototype_vars = opts || {};
+ 
+    // Add extra methods to the prototype
+    $.extend(prototype_vars, {
+      transmit: function(event_name){
+        $().trigger(name+'.'+event_name, this);
+      }
+    });
+ 
+    // Define class with prototype methods and attributes
+    var klass = function(){};
+    $.extend(klass.prototype, prototype_vars);
+ 
+    // Define the plugin
+    $.fn[name] = function(){
+    
+      var selector = $(this).selector;
+      var n = $(this).length; // Number of elements matching selector
+      var args = arguments;
+      
+      // Store which selector was used to apply which objectify object
+      $.objectifications.push([selector, name]);
+    
+      $(this).each(function(i){
+        // Instantiate the object and add instance methods/attributes to it
+        var obj = new klass();
+        
+        obj.elem = this; // Make the dom element available for use in object methods via 'this.elem'
+        obj.jelem = $(this); // Make the jquerified dom element available for use in object methods via 'this.jelem'
+        
+        this.obj = obj; // Make the obj available for use in jquery methods via 'this.obj'
+ 
+        // Add some useful meta-data to the object
+        obj.meta = {
+          obj_index: i,
+          total_objs: n,
+          selector: selector
+        };
+ 
+        // Call the init function if it exists
+        if(obj.init) obj.init.apply(obj, args);
+      });
+    
+      return $(this);
+    };
+  
+  };
+ 
+  // Connect is a wrapper around bind for global events, that connects objectified objects directly
+  $.connect = function(global_event, obj2_name, func){
+    var selector, name;
+    for(i in $.objectifications){
+      selector = $.objectifications[i][0];
+      name = $.objectifications[i][1];
+      if(name == obj2_name){
+        $().bind(global_event, function(evt, obj1){
+          $(selector).each(function(){
+            func.call(this, obj1, this.obj);
+          });
+        });
+      }
+    }
+  };
+ 
+})(jQuery);
---
+++ run.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+require 'cramp/controller'
+
+Cramp::Controller::Websocket.backend = :thin
+
+class Connection
+  
+  class << self
+    def add(websocket_connection)
+      connections << websocket_connection
+    end
+  
+    def all
+      connections
+    end
+    
+    def remove(websocket_connection)
+      connections.delete(websocket_connection)
+    end
+    
+    private
+    def connections
+      @connections ||= []
+    end
+  end
+  
+end
+
+class WebsocketController < Cramp::Controller::Websocket
+  
+  on_start :add_connection
+  on_finish :remove_connection
+  
+  on_data :received_data
+
+  def add_connection
+    Connection.add(self)
+  end
+  
+  def remove_connection
+    Connection.remove(self)
+  end
+
+  def received_data(data)
+    Connection.all.each{|ws| ws.render(data) unless ws == self }
+  end
+  
+end
+
+Rack::Handler::Thin.run WebsocketController, :Port => 3001
